b85164118c0e0c8bbb8c56055ced4f8db12894a8
Luiza <luiza.pinheiro@telessauders.ufrgs.br>
2019-10-30 09:16:37 -0300

    Added icon button and base

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,96 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-hover v-slot:default="{ hover }">
+      <img
+        :src="
+          `${
+            hover
+              ? require('../assets/tetora2.png')
+              : require('../assets/tetora1.png')
+          }`
+        "
+      />
+    </v-hover>
+    <h1>Components go here:</h1>
+    <br />
+    <h2>Button time!!</h2>
+    <v-container>
+      <v-row align="center" justify="center">
+        <mIconButton
+          cor="#DF380F"
+          icone="mdi-cancel"
+          texto="Cancelar"
+          :func="funcao"
+          value="Eu cancelo coisas"
+        />
+        <mIconButton
+          cor="#6F53E9"
+          icone="mdi-printer"
+          texto="Visualizar impressão"
+          :func="funcao"
+        />
+        <mIconButton
+          cor="#FFC400"
+          icone="mdi-calendar"
+          texto="Agendar retorno"
+          :func="funcao"
+        />
+        <mIconButton
+          cor="#008C88"
+          icone="mdi-arrow-right"
+          texto="Transferir"
+          :func="funcao"
+        />
+        <mIconButton
+          cor="#6F53E9"
+          icone="mdi-plus-circle-outline"
+          texto="Novo processo"
+          :func="funcao"
+        />
+        <mIconButton
+          cor="#FFC400"
+          icone="mdi-content-save"
+          texto="Salvar"
+          :func="funcao"
+        />
+        <mIconButton
+          cor="#008C88"
+          icone="mdi-check-circle"
+          texto="Finalizar"
+          :func="funcao"
+        />
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import mIconButton from "@/views/m_icon_button.vue";
 export default {
   name: "home",
   components: {
-    HelloWorld
+    mIconButton
+  },
+  methods: {
+    funcao(coisa) {
+      coisa ? alert(coisa) : alert("Fiz algo");
+    }
   }
 };
 </script>
+
+<style>
+img {
+  max-width: 350px;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+}
+
+h2,
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/views/m_icon_button.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: "mIconButton",
+  props: {
+    cor: String,
+    icone: String,
+    texto: String,
+    func: Function,
+    value: String
+  }
+};
+</script>
+
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <v-hover v-slot:default="{ hover }">
+        <v-btn
+          text
+          icon
+          id="sla"
+          v-on="on"
+          :color="`${hover ? cor : 'gray'}`"
+          v-on:click="func(value)"
+        >
+          <v-icon> {{ icone }}</v-icon>
+        </v-btn>
+      </v-hover>
+    </template>
+    <span> {{ texto }}</span>
+  </v-tooltip>
+</template>
